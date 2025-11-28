09f611d576da91bbe3c3e2de1301df3377763f52
John Doe <johndoe@example.com>
2025-11-28 12:04:38 +0100

    Change Canvas Size

--- game.js
+++ game.js
@@ -2,41 +2,41 @@ import { character } from "./character";
 import platform from "platform";
 
 function setup() {
-    createCanvas(400, 400);
+  createCanvas(360, 580);
 }
 
 // Obstacle / Spike / Death
 function drawObstacle() {
-    push();
-    fill("red");
-    triangle(180, 300, 210, 240, 240, 300);
-    pop();
+  push();
+  fill("red");
+  triangle(180, 300, 210, 240, 240, 300);
+  pop();
 }
 
 let x = 100;
 let y = 100;
 
 function draw() {
-    background(100, 100, 100);
+  background(100, 100, 100);
 
-    character.draw();
-	platform.draw();
+  character.draw();
+  platform.draw();
 
-    platform.x -= 10;
-    if(platform.x + platform.w < 0){
-        platform.x = 500;
-    }
+  platform.x -= 10;
+  if (platform.x + platform.w < 0) {
+    platform.x = 500;
+  }
 
-    if(character.y + character.h < 300){
-        character.y += 10;
-    }
+  if (character.y + character.h < 300) {
+    character.y += 10;
+  }
 
-    // Floor
-    line(0, 300, 400, 300);
+  // Floor
+  line(0, 300, 400, 300);
 }
 
-function keyPressed(){
-    if(character.y + character.h === 300){
-        character.y -= 80;
-    }
+function keyPressed() {
+  if (character.y + character.h === 300) {
+    character.y -= 80;
+  }
 }
