753edca9434871df4eff23fdd18ef73ec6b8a2dc
John Doe <johndoe@example.com>
2025-11-28 13:27:24 +0100

    ca

--- character.js
+++ character.js
@@ -1,9 +1,12 @@
 export let character = {
-    x: 50,
-    y: 50,
-    w: 50,
-    h: 50,
-    draw() {
-        rect(this.x, this.y, this.w, this.h);
-    },
+  x: 50,
+  y: 50,
+  w: 50,
+  h: 50,
+  draw() {
+    push();
+    fill("white");
+    rect(this.x, this.y, this.w, this.h);
+    pop();
+  },
 };
